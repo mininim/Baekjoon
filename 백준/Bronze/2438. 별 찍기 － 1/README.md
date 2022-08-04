d83c525970f409ca50ce5fdb3fa82584ea5a56db
Minsop Lee <77574120+mininim@users.noreply.github.com>
2022-08-04 10:14:10 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 별 찍기 - 1 - 2438 
+
+[문제 링크](https://www.acmicpc.net/problem/2438) 
+
+### 성능 요약
+
+메모리: 69100 KB, 시간: 12 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.swift
@@ -0,0 +1,13 @@
+let input : Int = Int(readLine()!)!
+
+for i in 1...input{
+    
+    var x : String = ""
+    
+    for _ in 1...i{
+            x += "*"
+    }
+    
+    print(x)
+    
+}
