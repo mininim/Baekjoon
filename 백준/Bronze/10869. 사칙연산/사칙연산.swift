32c77a53fa0d04c1bae18d302e0b3322002880ca
Minsop Lee <77574120+mininim@users.noreply.github.com>
2022-08-06 11:54:42 +0900

    [Bronze V] Title: 사칙연산, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,7 @@
+let input = readLine()!.split(separator: " ").map { Int($0)!}
+
+print( input[0] + input[1] )
+print( input[0] - input[1] )
+print( input[0] * input[1] )
+print( input[0] / input[1] )
+print( input[0] % input[1] )
