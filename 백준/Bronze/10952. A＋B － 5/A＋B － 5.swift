fff87673570b937e00fba13dec1640ec7f49b087
Minsop Lee <77574120+mininim@users.noreply.github.com>
2022-08-06 13:30:49 +0900

    [Bronze V] Title: A+B - 5, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+ 
+while true {
+    
+    let num = readLine()!.split(separator: " ").map { Int($0)!}
+    if ( (num[0] == 0) && num[1] == 0)  {
+        break
+    }
+    print(num[0] + num[1])
+}
+
