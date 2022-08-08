f72879591afe015df3c94de42a101ef303d04446
Minsop Lee <77574120+mininim@users.noreply.github.com>
2022-08-08 12:28:01 +0900

    [Silver IV] Title: 큐, Time: 28 ms, Memory: 79652 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+var queue: [Int] = []
+
+func push(_ a: Int) {
+    queue.append(a)
+}
+
+func size() -> Int {
+    return queue.count
+}
+
+func empty() -> Int {
+    if queue.isEmpty {
+        return 1
+    }else {
+        return 0
+    }
+}
+
+func pop() -> Int {
+    if size() == 0 {
+        return -1
+    }else {
+        return queue.removeFirst()
+    }
+}
+
+func front() -> Int {
+    if size() == 0 {
+        return -1
+    }else {
+        return queue[0]
+    }
+}
+
+func back() -> Int {
+    if size() == 0 {
+        return -1
+    }else {
+        return queue[size()-1]
+    }
+}
+
+
+let n = Int(readLine()!)!
+
+for _ in 1...n {
+    let a = readLine()!.split(separator: " ")
+    switch String(a[0]) {
+    case "push":
+        push(Int(a[1])!)
+    case "pop":
+        print(pop())
+    case "size":
+        print(size())
+    case "empty":
+        print(empty())
+    case "front":
+        print(front())
+    case "back":
+        print(back())
+    default:
+        break
+    }
+}
